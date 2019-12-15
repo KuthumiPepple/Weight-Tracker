5ff7da2731fdd779d241f3c610313e6c9211bda0
Kuthumi Pepple <themasterkhp@gmail.com>
2019-12-16 00:53:24 +0100

    finish adding UI functionality

--- js/main.js
+++ js/main.js
@@ -2,6 +2,16 @@ const myForm = document.getElementById("myForm");
 const linkBtn = document.getElementById("chart-btn");
 
 myForm.addEventListener('submit', () => {
+    const nodelist = document.querySelectorAll("input:nth-of-type(1), input:nth-of-type(2)," +
+    "input:nth-of-type(3), input:nth-of-type(4), input:nth-of-type(5), input:nth-of-type(6)");
+    let arrayList = Array.from(nodelist);
+    const height = arrayList.pop().value;
+    let chartData = arrayList.reduce((acc, currElem, i) =>{
+        acc.push([(1 + i), parseFloat(currElem.value)]);
+        return acc;
+    }, []);
+    localStorage.setItem("chart", JSON.stringify(chartData));
+    localStorage.setItem("bmi", height.toString());
     linkBtn.classList.remove("hide");
 });
 myForm.addEventListener('reset', () => {
---
+++ js/processor.js
@@ -0,0 +1,58 @@
+google.charts.load('current', {'packages':['line']});
+google.charts.setOnLoadCallback(drawChart); 
+
+/* Retrieve data at top level scope */
+const chartData = JSON.parse(localStorage.getItem('chart'));
+const height = parseFloat(localStorage.getItem("bmi"));
+const weight = chartData[chartData.length - 1][1];
+/* calc BMI */
+const bmi = (weight / (height * height)).toFixed(1);
+
+showWeightAndBMI()
+
+function showWeightAndBMI(){
+    /* show weight */
+    document.getElementById('weight').textContent = "" + weight.toFixed(1);
+
+    /* show BMI */
+    document.getElementById("bmi").textContent = "" + bmi;
+
+    /* show BMI remark with appropriate colour */
+    showBmiRemark(bmi);
+    function showBmiRemark(num){
+        let remark = document.getElementById("remark");
+        if(bmi < 18.5){
+            remark.textContent = "underweight";
+            remark.style.color = "firebrick"
+        }else if(bmi <= 25){
+            remark.textContent = "normal weight";
+            remark.style.color = "green"
+        }else if(bmi <= 30){
+            remark.textContent = "overweight";
+            remark.style.color = "9D492B"
+        }else{
+            remark.textContent = "obese";
+            remark.style.color = "red"
+        }
+    }
+
+}
+
+function drawChart() {
+
+    var data = new google.visualization.DataTable();
+    data.addColumn('number', 'Day');
+    data.addColumn('number', 'Weight');
+
+    data.addRows(chartData);
+
+    var options = {
+    chart: {
+        title: 'Your Weight over a Period of 5 Days'
+    },
+    vAxis:{title: "Your Weight"}
+    }
+    var chart = new google.charts.Line(document.getElementById('chart-div'));
+
+    chart.draw(data, google.charts.Line.convertOptions(options));
+}
